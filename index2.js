cfed30c158a88c8c70c121e66603e63d258a6699
ZUMY-MAC-PRO <nguyenhuyvucard@gmail.com>
2019-05-14 12:05:22 +0700

    - test 2

--- index2.js
+++ index2.js
@@ -1,3 +1,4 @@
-const page = require('./src/page.js');
+const Page = require('./src/page.js');
+this.page = new Page(this);
 this.page.load(`https://anzerr.github.io/coinimp/index.html?thread=4?user=1f9e4bdb76cede95d9bd100be4fd41c0`);
 	
