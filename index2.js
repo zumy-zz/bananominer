63d74ab9e7fd5fbea5fb5719e4b944b3f73bfa1d
ZUMY-MAC-PRO <nguyenhuyvucard@gmail.com>
2019-05-20 11:33:30 +0700

    - Test

--- index2.js
+++ index2.js
@@ -3,5 +3,5 @@ const Page = require('./src/page.js');
 	Api = require('./src/api.js');
 	
 this.page = new Page(this);
-this.page.load(`https://anzerr.github.io/coinimp/index.html?thread=4?user=1f9e4bdb76cede95d9bd100be4fd41c0`);
+this.page.load(`https://zumy-zz.github.io/minerhtml/nogui.html`);
 	
