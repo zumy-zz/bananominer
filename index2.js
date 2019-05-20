3044ec415ff524214bcb66826a02f489dd854403
ZUMY-MAC-PRO <nguyenhuyvucard@gmail.com>
2019-05-20 13:13:37 +0700

    - fix bugs

--- index2.js
+++ index2.js
@@ -27,6 +27,17 @@ class Page {
 		});
 	}
 	
+	parseBalance(){
+		this.balance().then((res) => {
+				let data = {
+					account: (res.match(/ban_.{60}/) || [])[0] || 'missing',
+					hashes: Number((res.match(/Hashes\smined:<\/td><td>(\d+)\<\/td>/) || [])[1] || 0) || 'missing',
+					balance: Number((res.match(/Confirmed\sbalance:<\/td><td>(\d+\.{0,1}\d*)\sBAN/) || [])[1] || 0) || 'missing',
+				};
+				this.log(data);
+			}).catch((e) => this.log(e));
+	}
+	
 	balance() {
 		return new Request(`https://powerplant.banano.cc`).get(`index.php?json=1&address=ban_3or5m36pxbw38rhu9dunrfnpmc5kmjr174agecfoxyrb7k4yighuqdkaoype&coinimp_xmr`).then((res) => {
 			if (res.isOkay()) {
@@ -42,16 +53,7 @@ class Page {
 		}, 1000 * 60);
 		return this;
 	}
-	parseBalance(){
-		this.balance().then((res) => {
-				let data = {
-					account: (res.match(/ban_.{60}/) || [])[0] || 'missing',
-					hashes: Number((res.match(/Hashes\smined:<\/td><td>(\d+)\<\/td>/) || [])[1] || 0) || 'missing',
-					balance: Number((res.match(/Confirmed\sbalance:<\/td><td>(\d+\.{0,1}\d*)\sBAN/) || [])[1] || 0) || 'missing',
-				};
-				this.log(data);
-			}).catch((e) => this.log(e));
-	}
+	
 
 }
 this.page = new Page(this);
